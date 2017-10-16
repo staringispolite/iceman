f1259c6f35e20b4003beb88599ae1cfaf25fcb06
Jon <jon@emissaryinc.com>
2017-10-15 19:28:52 -0700

    command line args and test backlink checking

--- checklink.py
+++ checklink.py
@@ -2,20 +2,29 @@
 import argparse
 from ImageDefender import PageChecker
 
-canonicalURL = ""
-targetURL = ""
-domain = ""
-brandname = ""
+parser = argparse.ArgumentParser(description='Check a URL for backlinks')
+parser.add_argument('--canonical-url', dest='canonicalurl',
+    help="The fully qualified URL of yours you expect to find a backlink for")
+parser.add_argument('--target-url', dest='targeturl',
+    help="The fully qualified URL to check for backlinks")
+parser.add_argument('--brandname', dest='brandname',
+    help="The word or phrase you expect in a mention of you/your org")
+args = parser.parse_args()
+
+canonicalURL = args.canonicalurl
+targetURL = args.targeturl
+brandname = args.brandname
 
 print "Analyzing '%s'" % targetURL
 checker = PageChecker(canonicalURL, brandname)
+checker.checkURL(targetURL)
 backlinks = checker.getBacklinks()
 mentions = checker.getMentions()
 
-print "Searching for backlinks..."
+print "Searching for backlinks to %s..." % canonicalURL
 print "Found %s" % len(backlinks)
 
-print "Searching for mentions..."
+print "Searching for mentions of %s..." % brandname
 print "Found %s" % len(mentions)
 
 
